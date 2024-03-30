6766c42031b16a1352cf955d70d05acc515b48c1
Seyed Danial Movahed <movaheddanial@gmail.com>
2024-03-30 03:43:04 +0330

    Testing in android

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import subprocess
 import socketio
 import psutil
 import os
+import random
 
 sio = socketio.Client()
 
-name = "Piran"
+name = "Rostam"
 
 
 @sio.event
@@ -22,13 +23,48 @@ def name_accepted():
         sio.emit("Monitor", {
             'name': name,
             'cpu': psutil.cpu_percent(0.5),
-            'loadAvg': format(os.getloadavg()[1] , '.2f'),
+            'loadAvg': random.randint(0,20),
             'ram': format(psutil.virtual_memory()[2], '.1f'),
-            'disk': round(disk.free/disk.total * 100, 2),
-            'usersCount': int(subprocess.check_output("who| awk '{print $1}'|sort -u| wc -l", shell=True).decode()),
-            'packageTemp': psutil.sensors_temperatures()['coretemp'][0].current
+            'disk': round(100-(disk.free/disk.total * 100), 2),
+            'usersCount': random.randint(0,100),
+            'packageTemp': random.randint(0,100)
         })
-        
+        sio.emit("Monitor", {
+            'name': "Giv",
+            'cpu': random.randint(0,100),
+            'loadAvg': random.randint(0,20),
+            'ram': format(psutil.virtual_memory()[2], '.1f'),
+            'disk': round(100-(disk.free/disk.total * 100), 2),
+            'usersCount': random.randint(0,100),
+            'packageTemp': random.randint(0,100)
+        })
+        sio.emit("Monitor", {
+            'name': "Esfandiar",
+            'cpu': random.randint(0,100),
+            'loadAvg': random.randint(0,20),
+            'ram': format(psutil.virtual_memory()[2], '.1f'),
+            'disk': round(100-(disk.free/disk.total * 100), 2),
+            'usersCount': random.randint(0,100),
+            'packageTemp': random.randint(0,100)
+        })
+        sio.emit("Monitor", {
+            'name': "Piran",
+            'cpu': random.randint(0,100),
+            'loadAvg': random.randint(0,20),
+            'ram': format(psutil.virtual_memory()[2], '.1f'),
+            'disk': round(100-(disk.free/disk.total * 100), 2),
+            'usersCount': random.randint(0,100),
+            'packageTemp': random.randint(0,100)
+        })
+        sio.emit("Monitor", {
+            'name': "Roudabeh",
+            'cpu': random.randint(0,100),
+            'loadAvg': random.randint(0,20),
+            'ram': format(psutil.virtual_memory()[2], '.1f'),
+            'disk': round(100-(disk.free/disk.total * 100), 2),
+            'usersCount': random.randint(0,100),
+            'packageTemp': random.randint(0,100)
+        })
 
 
 @sio.event
@@ -36,5 +72,5 @@ def disconnect():
     print('disconnected from server')
 
 
-sio.connect('https://complexmonitor.darkube.app')
+sio.connect('http://localhost:3333')
 sio.wait()
