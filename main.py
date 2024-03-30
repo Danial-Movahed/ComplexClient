86d71c86900d211fd3368a7ef812bd88001ed4d0
Seyed Danial Movahed <movaheddanial@gmail.com>
2024-03-30 03:46:03 +0330

    Change name to get from hostname

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import subprocess
 import socketio
 import psutil
 import os
+import socket
 
 sio = socketio.Client()
 
-name = "Piran"
+name = socket.gethostname()
 
 
 @sio.event
@@ -24,7 +25,7 @@ def name_accepted():
             'cpu': psutil.cpu_percent(0.5),
             'loadAvg': format(os.getloadavg()[1] , '.2f'),
             'ram': format(psutil.virtual_memory()[2], '.1f'),
-            'disk': round(disk.free/disk.total * 100, 2),
+            'disk': round(100-(disk.free/disk.total * 100), 2),
             'usersCount': int(subprocess.check_output("who| awk '{print $1}'|sort -u| wc -l", shell=True).decode()),
             'packageTemp': psutil.sensors_temperatures()['coretemp'][0].current
         })
