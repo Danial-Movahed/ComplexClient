0c30bdecf41f8b3aa951d14a9e970f4a39785121
Seyed Danial Movahed <movaheddanial@gmail.com>
2024-03-30 06:04:20 +0330

    Added internet logged in user to main py and changed nc to no one

--- checkInternetUser.sh
+++ checkInternetUser.sh
@@ -1,7 +1,7 @@
 wget --spider --quiet http://example.com
 
 if [ "$?" != 0 ]; then
-    echo -n "NC"
+    echo -n "No one"
 else
     out=$(curl http://hotspot.sbu.ac.ir/status 2>/dev/null | grep "خوش آمدید" | awk '{print $5}')
     user=${out%"</div><br>"}
--- main.py
+++ main.py
@@ -20,6 +20,9 @@ def connect():
 def name_accepted():
     while True:
         disk = shutil.disk_usage("/")
+        InternetLoggedInUser = ""
+        if name == "Piran":
+            InternetLoggedInUser = subprocess.check_output("./checkInternetUser.sh", shell=True)
         sio.emit("Monitor", {
             'name': name,
             'cpu': psutil.cpu_percent(0.5),
@@ -27,7 +30,8 @@ def name_accepted():
             'ram': format(psutil.virtual_memory()[2], '.1f'),
             'disk': round(100-(disk.free/disk.total * 100), 2),
             'usersCount': int(subprocess.check_output("who| awk '{print $1}'|sort -u| wc -l", shell=True).decode()),
-            'packageTemp': psutil.sensors_temperatures()['coretemp'][0].current
+            'packageTemp': psutil.sensors_temperatures()['coretemp'][0].current,
+            "netLoggedIn": InternetLoggedInUser
         })
         
 
